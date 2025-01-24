ac023f16e9106a91e6db01fd02a3edc149ab50ea
Adam Kozakiewicz <adamkozakiewicz44@gmail.com>
2025-01-24 02:07:38 +0100

    Update scooter-sharing.sql

--- scooter-sharing.sql
+++ scooter-sharing.sql
@@ -1,1 +1,24 @@
-dd
+CREATE TABLE `scooter-sharing44.2024.ALL` AS
+SELECT * FROM `scooter-sharing44.2024.01`
+UNION ALL
+SELECT * FROM `scooter-sharing44.2024.02`
+UNION ALL
+SELECT * FROM `scooter-sharing44.2024.03`
+UNION ALL
+SELECT * FROM `scooter-sharing44.2024.04`
+UNION ALL
+SELECT * FROM `scooter-sharing44.2024.05`
+UNION ALL
+SELECT * FROM `scooter-sharing44.2024.06`
+UNION ALL
+SELECT * FROM `scooter-sharing44.2024.07`
+UNION ALL
+SELECT * FROM `scooter-sharing44.2024.08`
+UNION ALL
+SELECT * FROM `scooter-sharing44.2024.09`
+UNION ALL
+SELECT * FROM `scooter-sharing44.2024.10`
+UNION ALL
+SELECT * FROM `scooter-sharing44.2024.11`
+UNION ALL
+SELECT * FROM `scooter-sharing44.2024.12`
