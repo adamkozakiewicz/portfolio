a24e4640f56738f1ab84fe00b3ea28f34b522edd
Adam Kozakiewicz <adamkozakiewicz44@gmail.com>
2025-01-31 16:37:55 +0100

    Update scooter-sharing.sql

--- scooter-sharing.sql
+++ scooter-sharing.sql
@@ -1,24 +1,23 @@
-CREATE TABLE `scooter-sharing44.2024.ALL` AS
-SELECT * FROM `scooter-sharing44.2024.01`
-UNION ALL
-SELECT * FROM `scooter-sharing44.2024.02`
-UNION ALL
-SELECT * FROM `scooter-sharing44.2024.03`
-UNION ALL
-SELECT * FROM `scooter-sharing44.2024.04`
-UNION ALL
-SELECT * FROM `scooter-sharing44.2024.05`
-UNION ALL
-SELECT * FROM `scooter-sharing44.2024.06`
-UNION ALL
-SELECT * FROM `scooter-sharing44.2024.07`
-UNION ALL
-SELECT * FROM `scooter-sharing44.2024.08`
-UNION ALL
-SELECT * FROM `scooter-sharing44.2024.09`
-UNION ALL
-SELECT * FROM `scooter-sharing44.2024.10`
-UNION ALL
-SELECT * FROM `scooter-sharing44.2024.11`
-UNION ALL
-SELECT * FROM `scooter-sharing44.2024.12`
+/* Creating a new table with all rides from 2019 */
+
+create table scooter-sharing44.trips.2019 as
+    (
+    select * from scooter-sharing44.trips.q1
+    union all
+    select * from scooter-sharing44.trips.q2
+    union all
+    select * from scooter-sharing44.trips.q3
+    union all
+    select * from scooter-sharing44.trips.q4
+    );
+
+
+/* Creating a new column for the weekday of the ride's start */
+
+alter table scooter-sharing44.trips.2019 add column day_of_week string;
+
+update scooter-sharing44.trips.2019 
+set day_of_week = format_date('%a', start_time) 
+where true; 
+
+
